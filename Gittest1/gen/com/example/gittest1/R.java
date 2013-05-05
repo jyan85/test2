7ba5a66bb337d63a6989f24c2a628f89950ebdb8
jyan85 <jyan85@gmail.com>
2013-05-05 16:18:52 +0100

    all views are added programmatically. Next is to make it dynamic.

--- Gittest1/gen/com/example/gittest1/R.java
+++ Gittest1/gen/com/example/gittest1/R.java
@@ -35,19 +35,23 @@ public final class R {
     public static final class layout {
         public static final int activity_display_message=0x7f030000;
         public static final int activity_main=0x7f030001;
+        public static final int fragment_add__text_question=0x7f030002;
     }
     public static final class menu {
         public static final int display_message=0x7f070000;
         public static final int main=0x7f070001;
     }
     public static final class string {
+        public static final int Add_Picture=0x7f050009;
         public static final int action_settings=0x7f050001;
-        public static final int advise_questionanswer=0x7f050004;
+        public static final int advise_answer=0x7f050005;
+        public static final int advise_question=0x7f050004;
         public static final int app_name=0x7f050000;
         public static final int but_Submit=0x7f050003;
+        public static final int hello_blank_fragment=0x7f050008;
         public static final int hello_world=0x7f050002;
-        public static final int insert_answers=0x7f050006;
-        public static final int title_activity_display_message=0x7f050005;
+        public static final int insert_answers=0x7f050007;
+        public static final int title_activity_display_message=0x7f050006;
     }
     public static final class style {
         /** 
--- Gittest1/src/com/example/gittest1/MainActivity.java
+++ Gittest1/src/com/example/gittest1/MainActivity.java
@@ -3,30 +3,71 @@ package com.example.gittest1;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.GestureDetector;
 import android.view.Menu;
 import android.view.View;
+import android.view.ViewGroup.LayoutParams;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.Toast;
 
 public class MainActivity extends Activity
-		{
-	// private GestureLibrary gestureLib;
-	// private static final String DEBUG_TAG = "Gestures";
-	public final static String EXTRA_MESSAGE = "com.example.myfirstapp.MESSAGE"; // Must be added to pass
-    private Button mButton1;
-    //	private GestureDetector gestureDetector;
-	// View.OnTouchListener gestureListener;																	//	an intent into a new activity (key to the EXTRAS0
+	{
+	private Button mButton1;
+	private Button mButtonAddPic;
+	private EditText Question;
+	private EditText AnswerA;
+	private EditText AnswerB; // need to dynamically generater this later
+	private LinearLayout layout;
+	private LinearLayout.LayoutParams layoutParams;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_main);
-//		// Used for setting listeners to layout buttons etc
-		// Future slide-submit will need a new object that incorporates EditText and Slider
-//		 mEditText1 = (EditText) findViewById(R.id.editText1);
-		// alternative to android.onClick="" in the layout xml. Good for keeping track of actions
-	    mButton1 = (Button) findViewById(R.id.submit_context); // links field with button in layout
+		// setContentView(R.layout.activity_main);
+		LayoutParams params =
+				new LinearLayout.LayoutParams(
+						LayoutParams.MATCH_PARENT,
+						LayoutParams.WRAP_CONTENT);
+		// Layout Params for Layout which holds the Views:
+		// specific to linearlayouts
+		layoutParams =
+				new LinearLayout.LayoutParams(
+						LayoutParams.MATCH_PARENT,
+						LayoutParams.WRAP_CONTENT);
+		
+		layout = new LinearLayout(this);
+		layout.setOrientation(LinearLayout.VERTICAL);
+		
+		// change to dynamic later;
+		Question = new EditText(this);
+		Question.setHint(R.string.advise_question);
+		Question.setLayoutParams(params);
+		layout.addView(Question);
+		
+		AnswerA = new EditText(this);
+		AnswerA.setHint(R.string.advise_answer);
+		AnswerA.setLayoutParams(params);
+		layout.addView(AnswerA);
+		
+		AnswerB = new EditText(this);
+		AnswerB.setHint(R.string.advise_answer);
+		AnswerB.setLayoutParams(params);
+		layout.addView(AnswerB);
+		
+		mButton1 = new Button(this); // links field with button in layout
+		mButton1.setText(R.string.but_Submit);
 	    mButton1.setOnClickListener(new Button_Event()); // new click listener activated (new event)
+	    mButton1.setLayoutParams(params);
+	    layout.addView(mButton1);
+	    
+	    mButtonAddPic = new Button(this);
+	    mButtonAddPic.setText(R.string.Add_Picture);
+	    mButtonAddPic.setOnClickListener(new Button_Event());
+	    mButtonAddPic.setLayoutParams(params);
+	    layout.addView(mButtonAddPic);
+	    
+	    this.addContentView(layout, layoutParams);
 	}
 class Button_Event implements Button.OnClickListener
 	{
@@ -37,72 +78,45 @@ class Button_Event implements Button.OnClickListener
 	       {
 	            bufferMessage(); 
 	       }  
+		  if(v==mButtonAddPic)
+		  {
+			  Toast.makeText(getBaseContext(), "Clicked",
+					 Toast.LENGTH_SHORT).show();
+//			  layout.addView(AnswerB);
+//			  Context d = getBaseContext()
+//			  d.addContentView(layout, layoutParams);
+		  }
 	  }  
 	}
-//	        //
-//		gestureDetector = new GestureDetector(this, new MyGestureDetector());
-//        gestureListener = new View.OnTouchListener() {
-//            public boolean onTouch(View v, MotionEvent event) {
-//                return gestureDetector.onTouchEvent(event);
-//            }
-//        };
-//		GestureOverlayView gestureOverlayView = new GestureOverlayView(this);
-//	    View inflate = getLayoutInflater().inflate(R.layout.activity_main, null);
-//	    gestureOverlayView.addView(inflate);
-//	    gestureOverlayView.addOnGesturePerformedListener(this);
-//	    gestureLib = GestureLibraries.fromRawResource(this, R.raw.gestures);
-//	    if (!gestureLib.load()) {
-//	      finish();
-//	    }
-//	    setContentView(gestureOverlayView);
-		    
-	
+		// What happens when you click on send    
 	public void bufferMessage() {
 		// create an intent to start an activity called DisplayMessageActivity
 		Intent submit = new Intent(this, DisplayMessageActivity.class);
 		// copies EditText object from inputs
-		EditText usertextQ = (EditText) findViewById(R.id.textQuestion);
-		EditText usertextA_choiceA = (EditText) findViewById(R.id.answer_a);
-		EditText usertextA_choiceB = (EditText) findViewById(R.id.answer_b);
+		// EditText usertextQ = (EditText) findViewById(R.id.textQuestion);
+		EditText usertextQ = Question;
+		EditText usertextA_choiceA = AnswerA;
+		EditText usertextA_choiceB = AnswerB;
 		// convert text in EditText object to String
 		String textQuestion = usertextQ.getText().toString();
-		// create a key for Bundle of text string
-//		String textQ = new String();
 		// add some int to Bundle
 		Bundle newQuipo = new Bundle();
-		
 		newQuipo.putString("textA_choiceA", usertextA_choiceA.getText().toString());
 		newQuipo.putString("textA_choiceB", usertextA_choiceB.getText().toString());
 		int line_length = 10;
-		// key for int to Bundle
-//		String length = new String();
-		// instance new Bundle
-		
 		// Put User inputed String into Bundle
 		newQuipo.putString("textQ", textQuestion);
 		// put the int into bundle
 		newQuipo.putInt("length", line_length);
 		// add Extras to the intent
-//		submit.putExtra(EXTRA_MESSAGE, newQuipo);
 		submit.putExtras(newQuipo);
-		// start new activity with submitted intent
 		startActivity(submit);
 	}
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
+
 		getMenuInflater().inflate(R.menu.main, menu);
 		return true;
-	}
-
-//class MyGestureDetector extends SimpleOnGestureListener
-//{
-//		@Override
-//		public boolean onDoubleTap(MotionEvent e) {
-//			bufferMessage();
-//			return true;
-//		}
-//}
-}
+	}}
 
 
